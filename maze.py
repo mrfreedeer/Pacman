2b4573e8525c93089d100397498a9211de4c9319
mrfreedeer <31293109+mrfreedeer@users.noreply.github.com>
2017-10-19 19:23:39 -0500

    Add files via upload
    Corrected maze, now drawn using Surfaces and related to Sprites for further use

--- maze.py
+++ maze.py
@@ -1,5 +1,6 @@
 import pygame
 import json
+import math
 from pygame.locals import *
 from sys import exit
 
@@ -33,46 +34,49 @@ class Yline (pygame.sprite.Sprite):
 
 
 
-def readmaze():
-    json_data = open("C:\\Users\\Usuario1\\Desktop\\Pacman-master\\Pacman-master\\maze.txt").read().split("\n")
+def readmaze(string):
+    json_data = open(string).read().split("\n")
     return json_data
 class Maze(pygame.sprite.Sprite):
-    def __init__(self, TILESIZE, DOCK):
+    def __init__(self, string, free, height, DOCK):
         pygame.sprite.Sprite.__init__(self)
-        self._t = TILESIZE
+
         self._d = DOCK
-        self._mazemap = readmaze()
+        self._mazemap = readmaze(string)
         self._width = len(self._mazemap[0])
         self._height = len(self._mazemap) - 1
+        self._t = math.ceil(abs((height - free)) / self._height)
+        print(self._t)
         self._walls = []
         self._lines = []
         self._sprites = pygame.sprite.Group()
-         
+
 
     def outline(self, pantalla):
         self._lines.append(Xline(self._d[0], self._d[1], self._width * self._t))
         self._lines.append(Xline(self._d[0], self._height*self._t + self._d[1] +self._t,self._width * self._t ))
         self._lines.append(Yline(self._d[0], self._d[1],self._t * 6))
         self._lines.append(Yline(self._d[0] + self._width * self._t, self._d[1],self._t * 6))
-        self._lines.append(Yline(self._d[0], self._d[1] + 13 * self._t,self._t * 20))
-        self._lines.append(Yline(self._d[0] + self._width * self._t, self._d[1] + 13 * self._t, self._t * 20))
 
-        self._lines.append(Yline(self._d[0] + 3 * self._t, self._d[1]+ 6 * self._t,9 * self._t))
-        self._lines.append(Yline(self._d[0] + 3 * self._t, self._d[1]+ 10 * self._t,13 * self._t))
+        self._lines.append(Yline(self._d[0], self._d[1] + 13 * self._t,self._t * 7))
+        self._lines.append(Yline(self._d[0] + self._width * self._t, self._d[1] + 13 * self._t, self._t * 7))
 
-        self._lines.append(Yline(self._d[0] + (self._width - 3) * self._t, self._d[1]+ 6 * self._t,9 * self._t))
-        self._lines.append(Yline(self._d[0] + (self._width - 3) * self._t, self._d[1]+ 10 * self._t, 13 * self._t))
+        self._lines.append(Yline(self._d[0] + 3 * self._t, self._d[1]+ 6 * self._t,3 * self._t))
+        self._lines.append(Yline(self._d[0] + 14 * self._t, self._d[1]+ 6 * self._t,3 * self._t))
 
-        self._lines.append(Yline(self._d[0] + 6 * self._t, self._d[1] + 8 * self._t, 11 * self._t))
-        self._lines.append(Yline(self._d[0] + 11 * self._t, self._d[1] + 8 * self._t,  11 * self._t))
-    
+        self._lines.append(Yline(self._d[0] + 3 * self._t, self._d[1]+ 10 * self._t,3 * self._t))
+        self._lines.append(Yline(self._d[0] + 14 * self._t, self._d[1]+ 10 * self._t,3 * self._t))
+
+        self._lines.append(Yline(self._d[0] + 6 * self._t, self._d[1] + 8 * self._t, 3 * self._t))
+        self._lines.append(Yline(self._d[0] + 11 * self._t, self._d[1] + 8 * self._t,  3 * self._t))
+
     def draw(self, pantalla):
         self.outline(pantalla)
         for row in range(self._height):
             for col in range(self._width):
                 if self._mazemap[row][col] == '1' :
                     #pygame.draw.rect(pantalla, blue,(col*self._t + self._d[0], row*self._t + self._d[1], self._t, self._t))
-                    self._walls.append(Wall(col*self._t + self._d[0], row*self._t + self._d[1], self._t)) 
+                    self._walls.append(Wall(col*self._t + self._d[0], row*self._t + self._d[1], self._t))
                 if self._mazemap[row][col] == '0' :
                     pass
                 if self._mazemap[row][col] == '3':
@@ -83,3 +87,5 @@ class Maze(pygame.sprite.Sprite):
         for y in self._lines:
             self._sprites.add(y)
         self._sprites.draw(pantalla)
+    def getSprites(self):
+        return self._sprites
