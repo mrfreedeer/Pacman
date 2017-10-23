4de42c2cbdfe4ab1c8a67f2eeddb65b3b9da9bb1
mrfreedeer <31293109+mrfreedeer@users.noreply.github.com>
2017-10-23 17:41:21 -0500

    Add files via upload
    Modified to include a player, which has continous movement and one further movement memory

--- Pacman.py
+++ Pacman.py
@@ -1,5 +1,5 @@
 import pygame
-from maze import Maze
+from maze import *
 from pygame.locals import *
 
 red = (255,0,0)
@@ -11,6 +11,44 @@ black = (0,0,0)
 pink = (255,200,200)
 TILESIZE = 31
 
+def movement(key, speed):
+    if key == pygame.K_RIGHT:
+        jp.rect.x += speed
+        jp.posx += speed
+    if key == pygame.K_LEFT:
+        jp.rect.x -= speed
+        jp.posx -= speed
+    if key == pygame.K_UP:
+        jp.rect.y -= speed
+        jp.posy -= speed
+    if key == pygame.K_DOWN:
+        jp.rect.y += speed
+        jp.posy += speed
+def changedir(key, playershadow, jp):
+    if key == pygame.K_RIGHT:
+        move = True
+        playershadow.x = 5
+        playershadow.y = 0
+        playershadow.rect.x = jp.rect.x + 1
+        playershadow.rect.y = jp.rect.y
+    elif key == pygame.K_LEFT:
+        move = True
+        playershadow.x = -5
+        playershadow.y = 0
+        playershadow.rect.x = jp.rect.x - 1
+        playershadow.rect.y = jp.rect.y
+    elif key == pygame.K_UP:
+        move = True
+        playershadow.y = -5
+        playershadow.x = 0
+        playershadow.rect.x = jp.rect.x
+        playershadow.rect.y = jp.rect.y - 1
+    elif key == pygame.K_DOWN:
+        move = True
+        playershadow.y = 5
+        playershadow.x = 0
+        playershadow.rect.x = jp.rect.x
+        playershadow.rect.y = jp.rect.y + 1
 if __name__ == '__main__':
     pygame.init()
     screensize = pygame.display.Info()
@@ -24,9 +62,108 @@ if __name__ == '__main__':
     m = Maze(mazelocation, 200, screensize.current_h, DOCK)
     m.draw(pantalla)
     mazesprites = m.getSprites()
+    playersize = TILESIZE - 1
+    startx = DOCK[0] + 3 * TILESIZE
+    starty = DOCK[1] + 15 * TILESIZE - 15
 
+    jp = Jugador(playersize,playersize, DOCK, TILESIZE, startx, starty)
+    playershadow = Jugador(playersize,playersize, DOCK, TILESIZE, startx, starty)
+    g = pygame.sprite.Group()
+    g.add(jp)
+    ancholab = m.getWidth() + DOCK[0]
+    limitancho = ancholab * m.getTile()
+    s = 0
+    quit = False
+    eating = False
+    speed  = 2
+    magic = pygame.Surface((TILESIZE,TILESIZE))
+    magic.fill(black)
+    move = False
+    key = None
+    turn = 0
+    turnspeed = 3
+    collision = False
+    pendingturn = False
+
+    mantain = pygame.K_LEFT
     while True:
-        pygame.display.flip()
+        pygame.draw.rect(pantalla, black, (jp.posx, jp.posy, playersize, playersize) )
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
+                exit()
+                quit = True
+        if event.type == pygame.KEYDOWN:
+            key = event.key
+            changedir(key, playershadow, jp)
+            collision = False
+            ls = pygame.sprite.spritecollideany(playershadow, mazesprites, False)
+            if ls != None:
+                collision = True
+                if mantain != key:
+                    pendingturn = True
+            else:
+                if pendingturn:
+                    mantain = key
+                    pendingturn = False
+                    print("turnnow")
+            if not collision:
+                mantain = key
+                pendingturn = False
+                if mantain == pygame.K_RIGHT:
+                    move = True
+                    jp.x = 5
+                elif mantain == pygame.K_LEFT:
+                    move = True
+                    jp.x = -5
+                elif mantain == pygame.K_UP:
+                    move = True
+                    jp.y = -5
+                elif mantain == pygame.K_DOWN:
+                    move = True
+                    jp.y = 5
+                if mantain == pygame.K_DOWN or mantain == pygame.K_UP:
+                    jp.x = 0
+
+                elif mantain == pygame.K_LEFT or mantain == pygame.K_RIGHT:
+                    jp.y = 0
+
+        if move and turn >= turnspeed:
+            if eating:
+                realspeed = speed * .5
+            elif (jp.posx < DOCK[0] + 4 * TILESIZE or jp.posx > DOCK[0] + (m.getWidth() - 4) * TILESIZE) and (jp.posy >= TILESIZE * 10 and jp.posy <= TILESIZE * 11):
+                realspeed = speed * 2
+            else:
+                realspeed = speed
+            movement(mantain, realspeed)
+            turn = 0
+
+
+        #m.draw(pantalla)
+        if not quit:
+            g.update(mazesprites, m.getWidth())
+            jp.update(mazesprites, m.getWidth())
+            changedir(key, playershadow, jp)
+
+
+            g.draw(pantalla)
+            pantalla.blit(magic, (DOCK[0] - 18 + TILESIZE * m.getWidth() , DOCK[1] + 9 * TILESIZE - 10))
+            pantalla.blit(magic, (DOCK[0] - TILESIZE , DOCK[1] + 9 * TILESIZE - 10))
+            pygame.display.flip()
+            if key == pygame.K_UP:
+                print("up")
+            elif key == pygame.K_DOWN:
+                print("down")
+            if key == pygame.K_LEFT:
+                print("left")
+            if key == pygame.K_RIGHT:
+                print("right")
+            print(collision)
+
+
+
+        turn += 1
+
+        #pos = pygame.mouse.get_pos()
+        #print(pos)
--- maze.py
+++ maze.py
@@ -13,6 +13,44 @@ white = (255,255,255)
 black = (0,0,0)
 pink = (255,200,200)
 
+class Jugador(pygame.sprite.Sprite):
+    def __init__(self,ancho, alto, dock, tile, startx, starty):
+        pygame.sprite.Sprite.__init__(self)
+        self._d = dock
+        self._t = tile
+        self.image = pygame.Surface([ancho,alto])
+        self.rect = self.image.get_rect()
+        self.rect.x = startx
+        self.rect.y = starty
+        self.x = 0
+        self.y = 0
+        self.posx = startx
+        self.posy = starty
+        self.image.fill(pink)
+    def update(self, mazesprites, width):
+        ls_col = pygame.sprite.spritecollide(self, mazesprites, False)
+        cols = False
+        for colision in ls_col:
+            cols = True
+            if self.x> 0:
+                self.rect.right = colision.rect.left
+            if self.x< 0:
+                self.rect.left = colision.rect.right
+        self.posx = self.rect.x
+
+        for colision in ls_col:
+            if self.y > 0:
+                self.rect.bottom = colision.rect.top
+            if self.y < 0:
+                self.rect.top = colision.rect.bottom
+        self.posy = self.rect.y
+
+        if self.posx < (self._d[0] - self._t + 5):
+            self.posx =  self._d[0] + self._t * (width -1)
+            self.rect.x = self._d[0] + self._t * (width -1)
+        if self.posx > self._d[0] + self._t * (width - 1)  :
+            self.posx = self.rect.x = self._d[0]
+
 class Wall (pygame.sprite.Sprite):
     def __init__(self, x, y, tilesize):
         pygame.sprite.Sprite.__init__(self)
@@ -46,46 +84,65 @@ class Maze(pygame.sprite.Sprite):
         self._width = len(self._mazemap[0])
         self._height = len(self._mazemap) - 1
         self._t = math.ceil(abs((height - free)) / self._height)
-        print(self._t)
+        #self._t = abs((height - free)) / self._height
         self._walls = []
         self._lines = []
         self._sprites = pygame.sprite.Group()
 
-
+    def getTile(self):
+        return self._t
+    def getWidth(self):
+        return self._width
     def outline(self, pantalla):
-        self._lines.append(Xline(self._d[0], self._d[1], self._width * self._t))
-        self._lines.append(Xline(self._d[0], self._height*self._t + self._d[1] +self._t,self._width * self._t ))
-        self._lines.append(Yline(self._d[0], self._d[1],self._t * 6))
+        self._lines.append(Xline(self._d[0] - 1, self._d[1] - 1, self._width * self._t))
+        self._lines.append(Xline(self._d[0] - 1, self._height*self._t + self._d[1] +self._t,self._width * self._t ))
+
+        self._lines.append(Yline(self._d[0] -1 , self._d[1],self._t * 6))
+
         self._lines.append(Yline(self._d[0] + self._width * self._t, self._d[1],self._t * 6))
 
-        self._lines.append(Yline(self._d[0], self._d[1] + 13 * self._t,self._t * 7))
+        self._lines.append(Yline(self._d[0] -1 , self._d[1] + 13 * self._t,self._t * 7))
         self._lines.append(Yline(self._d[0] + self._width * self._t, self._d[1] + 13 * self._t, self._t * 7))
 
-        self._lines.append(Yline(self._d[0] + 3 * self._t, self._d[1]+ 6 * self._t,3 * self._t))
-        self._lines.append(Yline(self._d[0] + 14 * self._t, self._d[1]+ 6 * self._t,3 * self._t))
 
-        self._lines.append(Yline(self._d[0] + 3 * self._t, self._d[1]+ 10 * self._t,3 * self._t))
-        self._lines.append(Yline(self._d[0] + 14 * self._t, self._d[1]+ 10 * self._t,3 * self._t))
+        self._lines.append(Yline(self._d[0] - 1 +  3 * self._t, self._d[1]+ 6 * self._t ,3 * self._t - 1))
+        self._lines.append(Yline(self._d[0] + 14 * self._t, self._d[1]+ 6 * self._t,3 * self._t - 1))
 
-        self._lines.append(Yline(self._d[0] + 6 * self._t, self._d[1] + 8 * self._t, 3 * self._t))
-        self._lines.append(Yline(self._d[0] + 11 * self._t, self._d[1] + 8 * self._t,  3 * self._t))
+        self._lines.append(Yline(self._d[0] - 1 + 3 * self._t, self._d[1]+ 10 * self._t,3 * self._t - 1))
+        self._lines.append(Yline(self._d[0] + 14 * self._t, self._d[1]+ 10 * self._t,3 * self._t - 1))
 
+
+        self._lines.append(Yline(self._d[0] + 6 * self._t, self._d[1] + 8 * self._t, 3 * self._t - 1))
+        self._lines.append(Yline(self._d[0] - 1 + 11 * self._t, self._d[1] + 8 * self._t,  3 * self._t - 1))
+
+        self._lines.append(Xline(self._d[0] - 1, self._d[1] + 6 * self._t, 3 * self._t))
+        self._lines.append(Xline(self._d[0] + 1 + self._t * (self._width - 3) , self._d[1]  + 6 * self._t, 3 * self._t))
+        self._lines.append(Xline(self._d[0], self._d[1] + 9 * self._t - 1, self._t * 3 ))
+        self._lines.append(Xline(self._d[0], self._d[1] + 10 * self._t , self._t * 3 ))
+
+        self._lines.append(Xline(self._d[0] + self._t * (self._width - 3), self._d[1] + 9 * self._t - 1, self._t * 3 ))
+        self._lines.append(Xline(self._d[0]+ self._t * (self._width - 3), self._d[1] + 10 * self._t, self._t * 3 ))
+        self._lines.append(Xline(self._d[0] + 1 + 6 * self._t, + self._d[1] + 8 * self._t, 2 * self._t))
+        self._lines.append(Xline(self._d[0] + 9 * self._t, + self._d[1] + 8 * self._t, 2 * self._t))
     def draw(self, pantalla):
         self.outline(pantalla)
         for row in range(self._height):
             for col in range(self._width):
                 if self._mazemap[row][col] == '1' :
-                    #pygame.draw.rect(pantalla, blue,(col*self._t + self._d[0], row*self._t + self._d[1], self._t, self._t))
                     self._walls.append(Wall(col*self._t + self._d[0], row*self._t + self._d[1], self._t))
                 if self._mazemap[row][col] == '0' :
                     pass
                 if self._mazemap[row][col] == '3':
-                    #pygame.draw.line(pantalla,blue,(col*self._t + self._d[0], row*self._t + self._d[1]), (col*self._t + self._d[0] + self._t,row*self._t + self._d[1]), 1)
-                    self._lines.append(Xline(col*self._t + self._d[0], row*self._t + self._d[1], self._t))
+                    self._lines.append(Xline(col*self._t + self._d[0], row*self._t + self._d[1] - 1, self._t))
         for x in self._walls:
             self._sprites.add(x)
         for y in self._lines:
             self._sprites.add(y)
         self._sprites.draw(pantalla)
+        z = Yline(self._d[0] - self._t - 5, self._d[1], self._t * self._height)
+        z1 = Yline(self._d[0] + self._t * (self._width + 1) + 5, self._d[1], self._t * self._height)
+        z2 = Xline(self._d[0] + 8 * self._t, self._d[1] + 8 * self._t, self._t)
+        self._sprites.add(z)
+        self._sprites.add(z1)
     def getSprites(self):
         return self._sprites
